7af5619577204f27ab15ffc507adee0a27c298ac
JonathanT10 <jonathan_terrell@yahoo.com>
2021-09-08 10:55:35 -0500

    Added navBar.js, footer.js and home.jsx set up basic outline for these files.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store';
+import Navbar from './src/components/navBar';
+import Footer from './src/components/footer';
+import Home from './components/home';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+  return(
+    <Provider store={store}>
+    <div>
+      <BrowserRouter>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/projects" component={Projects} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/contact" component={Contact} />
+      </Switch>
+      <Footer />
+      </BrowserRouter>
+    </div>
+    </Provider>
+  );
+  
+  }
+  
+  
+  export default App;
---
+++ src/components/home.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import '../components/css/home.css'
+import Dwig from '../images/comph.jpg'
+
+
+class Home extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return(
+            <div className="behind">
+                <div className="main">
+                    <h1>Micro Farms</h1>
+                    <h2>Full-Stack Developer</h2>
+                    <p className="opener">We should all be able to eat fresh food.</p>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Home;
