a79a4a57e753812db5fcf526ca4c010dca33b08a
JonathanT10 <jonathan_terrell@yahoo.com>
2021-09-14 11:20:26 -0500

    contact.jsx added form and function to submit form with email js

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import emailjs from 'emailjs-com';
 
 class Contact extends Component{
     constructor(props){
@@ -8,10 +9,39 @@ class Contact extends Component{
         }
     }
 
+    sendEmail(event) {
+        event.preventDefault();
+        emailjs.sendForm('')
+        .then((result) => {
+            console.log(result.text);
+        }, (error) => {
+            console.log(error.text);
+        });
+        event.target.reset()
+    }
+
+
+
     render() {
         return (
             <div>
                 <h1>Contact us here</h1>
+                <form onSubmit={this.sendEmail}>
+                    <div>
+                        <div>
+                            <input type="text" placeholder="Name" name="user_name"></input>
+                        </div>
+                        <div>
+                            <input type="email" placeholder="EmailAddress" name="user_email"></input>
+                        </div>
+                        <div>
+                            <textarea placeholder="Your Message" name="message"></textarea>
+                        </div>
+                        <div>
+                            <input type="submit" value="Send"></input>
+                        </div>
+                    </div>
+                </form>
             </div>
         )
     }
