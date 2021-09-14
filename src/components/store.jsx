bb7d55ff35ad604c7011af94c6dfc12de5522759
JonathanT10 <jonathan_terrell@yahoo.com>
2021-09-14 13:14:04 -0500

    store.jsx set up basic outline for the store page.

---
+++ src/components/store.jsx
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+
+class Store extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return(
+            <div>
+                <h1>Store</h1>
+            </div>
+        )
+    }
+}
+
+export default Store;
